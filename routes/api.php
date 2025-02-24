4addf3c5ac55e01e3f934ca30752f3d5f08d3e56
abhibs <abhirambs97@gmail.com>
2025-02-24 15:31:40 +0530

    contact model post api created

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\ChartController;
+use App\Http\Controllers\API\ContactController;
 use App\Http\Controllers\API\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +10,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/chart/datas', [ChartController::class, 'charts']);
 Route::get('/review/datas', [ReviewController::class, 'reviews']);
+Route::post('/contact/send', [ContactController::class, 'contactSend']);
