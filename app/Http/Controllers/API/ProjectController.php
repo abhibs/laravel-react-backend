a8d20a238228f6cd20f38933610f607c47fa8266
abhibs <abhirambs97@gmail.com>
2025-02-26 12:50:43 +0530

    project model home page api  created

---
+++ app/Http/Controllers/API/ProjectController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function homeProjects()
+    {
+        $datas = Project::inRandomOrder()->limit(3)->get();
+        return $datas;
+    }
+}
